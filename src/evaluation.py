5735f0ee2f7e829038550e29e015f06998c6ce30
Gabe <gabeorlanski@gmail.com>
2021-11-05 11:41:03 -0400

    Better metrics

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,7 @@
 import logging
 from collections import Counter, defaultdict
 from typing import Dict
-
+from math import isclose
 import numpy as np
 from tqdm import tqdm
 import torch
@@ -54,10 +54,10 @@ def evaluate(
     batches_seen = 0
 
     # Create a metric tracker for keeping track of metrics we use.
-    metric_tracker = Counter()
+    metric_tracker = defaultdict(list)
     description = ""
     for name, value in metric_tracker.items():
-        description += f"{name}: {0.0:.3f} "
+        description += f"{' ' if description else ''}{name}: {0.0:.3f}"
 
     data_iterator = tqdm(data_loader, desc=description)
     for batch in data_iterator:
@@ -83,38 +83,26 @@ def evaluate(
         # Pre initialize the preds nested array here because it saves time
         # later. Faster then appending.
         preds = [[None for _ in range(generated.shape[1])] for _ in range(len(gold))]
+        batch_metrics = {}
         for m in metrics:
-
-            # Calculate Oracle and actual
-            batch_metrics = {}
             for beam_num in range(generated.shape[1]):
                 # Decode only this beam
                 decoded_beams = tokenizer.batch_decode(
                     generated[:, beam_num, :],
                     skip_special_tokens=True
                 )
-                beam_metrics = METRICS_DICT[m](gold, decoded_beams)
+                if beam_num == 0:
+                    # only care about the metrics for the first beam.
+                    batch_metrics.update(METRICS_DICT[m](gold, decoded_beams))
                 for i, seq in enumerate(decoded_beams):
                     preds[i][beam_num] = seq
 
-                for metric, value in beam_metrics.items():
-                    # For the actual metric, we  only care about the first beam.
-                    if beam_num == 0:
-                        batch_metrics[metric] = value
-
-                    # Calculate the oracle metric and save it. Use default
-                    # value of -1 if it has not been set yet.
-                    batch_metrics[f"oracle_{metric}"] = max(
-                        value, batch_metrics.get(f"oracle_{metric}", -1)
-                    )
-            logger.debug(f"Metrics for batch {batches_seen}:")
-            for k, v in batch_metrics.items():
-                # nice formatting, has no other effect.
-                met_name = f"{k}:"
-                logger.debug(f"{met_name:>20} {v:.3f}")
-
-            for k, v in batch_metrics.items():
-                metric_tracker[k] += v
+        logger.debug(f"Metrics for batch {batches_seen}:")
+        for k, v in batch_metrics.items():
+            # nice formatting, has no other effect.
+            met_name = f"{k}:"
+            logger.debug(f"{met_name:>20} {v:.3f}")
+            metric_tracker[k].append(v)
 
         logger.debug("Saving JSON lines")
         for i, pred in enumerate(preds):
@@ -128,33 +116,27 @@ def evaluate(
         batches_seen += 1
         description = ""
         for name, value in metric_tracker.items():
-            description += f"{name}: {value / batches_seen:.3f} "
+            description += f"{' ' if description else ''}{name}: {np.mean(value):.3f}"
         data_iterator.set_description(
             description,
             refresh=True
         )
     data_iterator.close()
 
-    logger.info(f"Final scores for {task}:")
-    final_metrics = {}
-    for k, v in metric_tracker.items():
-        final_metrics[k] = v / batches_seen
-
-        # nice formatting, has no other effect.
-        met_name = f"{k}:"
-        logger.info(f"{met_name:>20} {final_metrics[k]:.3f}")
+    # logger.info(f"Final scores for {task}:")
+    # final_metrics = {}
+    # for k, v in metric_tracker.items():
+    #     final_metrics[k] = np.mean(v)
+    #     # nice formatting, has no other effect.
+    #     met_name = f"{k}:"
+    #     logger.info(f"{met_name:>20} {np.mean(v):.3f}")
 
     pred_file.close()
 
     logger.info("Finished evaluating the dataset with the prompt.")
 
-    metrics_file = out_path.joinpath("metrics.json")
-    logger.info(f"Saving metrics to '{metrics_file}'")
-    with metrics_file.open('w', encoding='utf-8') as fp:
-        fp.write(json.dumps(final_metrics, indent=True))
-
     # Sanity check
-    logger.info("Doing sanity check.")
+    logger.info("Doing sanity check and calculating Oracle.")
     targets = []
     predictions = []
     m_trackers = defaultdict(list)
@@ -172,13 +154,23 @@ def evaluate(
             for k, v in oracle.items():
                 m_trackers[k].append(v)
 
-    logger.info("Sanity Metrics:")
+    logger.info("Final Metrics:")
+    final_metrics = {}
     for m in metrics:
         for k, v in METRICS_DICT[m](targets, predictions).items():
             met_name = f"{k}:"
             logger.info(f"{met_name:>20} {v:.3f}")
+            assert isclose(v, np.mean(metric_tracker[k]))
+            logger.debug(f"Check passed for {k}")
+            final_metrics[k] = v
 
             met_name = f"oracle_{k}:"
-            logger.info(f"{met_name:>20} {np.mean(m_trackers[k]):.3f}")
+            final_metrics[met_name] = np.mean(m_trackers[k])
+            logger.info(f"{met_name:>20} {final_metrics[met_name]:.3f}")
+
+    metrics_file = out_path.joinpath("metrics.json")
+    logger.info(f"Saving metrics to '{metrics_file}'")
+    with metrics_file.open('w', encoding='utf-8') as fp:
+        fp.write(json.dumps(final_metrics, indent=True))
 
     return pred_path
