7d765a2060ec3d3d5bd0a8f93db0814e01c84f38
Gabe <gabeorlanski@gmail.com>
2021-11-06 15:27:57 -0400

    experiment batch

---
+++ experiments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Run the experiments
+python experiment.py task=copa model_name="bigscience/T0_3B"
+python experiment.py task="craigslist_bargains" model_name="bigscience/T0_3B"
+python experiment.py task=rte model_name="bigscience/T0_3B"
+python experiment.py task=wsc model_name="bigscience/T0_3B"
+python experiment.py task=wic model_name="bigscience/T0_3B"
