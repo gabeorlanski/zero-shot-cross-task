d21f8c5dd223cd65ab5dae80992a097afdbf6b2f
Gabe <gabeorlanski@gmail.com>
2021-11-14 11:48:44 -0500

    experiments

--- t0_experiments.sh
+++ t0_experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run the experiments
-python experiment.py length_normalization=True task=hellaswag +split=validation model_name="bigscience/T0_3B"
-python experiment.py force_generation=True task=hellaswag +split=validation model_name="bigscience/T0_3B"
+python experiment.py task=cb +split=validation num_proc=4 debug=False use_general_prompts=True
+python experiment.py task=anli +split=validation num_proc=4 debug=False use_general_prompts=True
