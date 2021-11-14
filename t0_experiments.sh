526524e49f2d84cddf732b9a714a979dba39f658
Gabe <gabeorlanski@gmail.com>
2021-11-14 11:50:12 -0500

    Model Fix

--- t0_experiments.sh
+++ t0_experiments.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # Run the experiments
-python experiment.py task=cb +split=validation num_proc=4 debug=False use_general_prompts=True
-python experiment.py task=anli +split=validation num_proc=4 debug=False use_general_prompts=True
+python experiment.py task=cb +split=validation \
+  num_proc=4 \
+  debug=False \
+  use_general_prompts=True \
+  model_name="bigscience/T0_3B"
+  
+python experiment.py task=anli +split=validation \
+  num_proc=4 \
+  debug=False \
+  use_general_prompts=True \
+  model_name="bigscience/T0_3B"
