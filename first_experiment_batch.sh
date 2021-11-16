0e408f9ed5b132c93e232d698ba711407a40f4bc
Gabe <gabeorlanski@gmail.com>
2021-11-16 11:29:56 -0500

    Better experiments

--- first_experiment_batch.sh
+++ first_experiment_batch.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Run the experiments
-python experiment.py +run_name="AC" task=cb +split=validation \
+python experiment.py +run_name="ACLN" task=cb +split=validation \
+  num_proc=4 \
+  debug=False \
+  use_general_prompts=True \
+  prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
+  evaluation.length_normalization=True \
+  model_name="bigscience/T0_3B"
+python experiment.py +run_name="ACNoLN" task=cb +split=validation \
+  num_proc=4 \
+  debug=False \
+  use_general_prompts=True \
+  prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
+  evaluation.length_normalization=False \
+  model_name="bigscience/T0_3B"
+python experiment.py +run_name="ACLN" task=anli +split=dev_r1 \
   num_proc=4 \
   debug=False \
   use_general_prompts=True \
@@ -8,15 +22,7 @@ python experiment.py +run_name="AC" task=cb +split=validation \
   evaluation.length_normalization=True \
   model_name="bigscience/T0_3B"
 
-python experiment.py +run_name="AC" task=anli +split=dev_r1 \
-  num_proc=4 \
-  debug=False \
-  use_general_prompts=True \
-  prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
-  evaluation.length_normalization=True \
-  model_name="bigscience/T0_3B"
-
-python experiment.py +run_name="AC" task=anli +split=dev_r2 \
+python experiment.py +run_name="ACLN" task=anli +split=dev_r2 \
   num_proc=4 \
   prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
   debug=False \
@@ -24,7 +30,7 @@ python experiment.py +run_name="AC" task=anli +split=dev_r2 \
   evaluation.length_normalization=True \
   model_name="bigscience/T0_3B"
 
-python experiment.py +run_name="AC" task=anli +split=dev_r3 \
+python experiment.py +run_name="ACLN" task=anli +split=dev_r3 \
   num_proc=4 \
   prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
   debug=False \
@@ -33,7 +39,31 @@ python experiment.py +run_name="AC" task=anli +split=dev_r3 \
   model_name="bigscience/T0_3B"
 
 
-python experiment.py +run_name="AC" task=cb +split=validation num_proc=4 \
+python experiment.py +run_name="ACNoLN" task=anli +split=dev_r1 \
+  num_proc=4 \
+  debug=False \
+  use_general_prompts=True \
+  prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
+  evaluation.length_normalization=False \
+  model_name="bigscience/T0_3B"
+
+python experiment.py +run_name="ACNoLN" task=anli +split=dev_r2 \
+  num_proc=4 \
+  prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
+  debug=False \
+  use_general_prompts=True \
+  evaluation.length_normalization=False \
+  model_name="bigscience/T0_3B"
+
+python experiment.py +run_name="ACNoLN" task=anli +split=dev_r3 \
+  num_proc=4 \
+  prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
+  debug=False \
+  use_general_prompts=True \
+  evaluation.length_normalization=False \
+  model_name="bigscience/T0_3B"
+
+python experiment.py +run_name="ACGen" task=cb +split=validation num_proc=4 \
   prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
   use_general_prompts=True \
   evaluation.force_generation=True \
