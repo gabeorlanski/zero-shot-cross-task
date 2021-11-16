4f6d5d79a46b19711e607305e33bbc12c3b0bf0e
Gabe <gabeorlanski@gmail.com>
2021-11-16 09:44:40 -0500

    New Experiments

--- t0_experiments.sh
+++ t0_experiments.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 # Run the experiments
 python experiment.py +run_name="Baseline" task=cb +split=validation model_name="bigscience/T0_3B"
-python experiment.py +run_name="Baseline" task=anli +split=dev_r1 model_name="bigscience/T0_3B"
-python experiment.py +run_name="Baseline" task=anli +split=dev_r2 model_name="bigscience/T0_3B"
-python experiment.py +run_name="Baseline" task=anli +split=dev_r3 model_name="bigscience/T0_3B"
-python experiment.py +run_name="Baseline" task=rte +split=validation model_name="bigscience/T0_3B"
-python experiment.py +run_name="Baseline" task=wsc +split=validation model_name="bigscience/T0_3B"
-python experiment.py +run_name="Baseline" task="wsc.fixed" +split=validation model_name="bigscience/T0_3B"
-python experiment.py +run_name="Baseline" task=wic +split=validation model_name="bigscience/T0_3B"
 
 python experiment.py +run_name="AC" task=cb +split=validation \
   num_proc=4 \
   debug=False \
   use_general_prompts=True \
+  evaluation.length_normalization=True \
   model_name="bigscience/T0_3B"
 
 python experiment.py +run_name="AC" task=anli +split=dev_r1 \
   num_proc=4 \
   debug=False \
   use_general_prompts=True \
+  evaluation.length_normalization=True \
   model_name="bigscience/T0_3B"
 
 python experiment.py +run_name="AC" task=anli +split=dev_r2 \
   num_proc=4 \
   debug=False \
   use_general_prompts=True \
+  evaluation.length_normalization=True \
   model_name="bigscience/T0_3B"
 
 python experiment.py +run_name="AC" task=anli +split=dev_r3 \
   num_proc=4 \
   debug=False \
   use_general_prompts=True \
+  evaluation.length_normalization=True \
   model_name="bigscience/T0_3B"
+  
+python experiment.py +run_name="AC" task=cb +split=validation num_proc=4 \
+  prompt_filter.name_list=["does it follow that","can we infer","justified in saying","should assume MCQ","support"] \
+  use_general_prompts=True \
+  evaluation.force_generation=True \
+  model_name="bigscience/T0_3B"
+
+
+
