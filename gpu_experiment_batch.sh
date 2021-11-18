02c9888e5c10e1ce38a223538f9aff823390e48c
Gabe <gabeorlanski@gmail.com>
2021-11-18 12:18:36 -0500

    Batch Updates

--- gpu_experiment_batch.sh
+++ gpu_experiment_batch.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 # Run the experiments
 
-python experiment.py +run_name="LowerCase" task=cb +split=validation \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-python experiment.py +run_name="LowerCase" task=anli +split=dev_r1 \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-python experiment.py +run_name="LowerCase" task=anli +split=dev_r2  \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-python experiment.py +run_name="LowerCase" task=anli +split=dev_r3  \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-python experiment.py +run_name="LowerCase" task=rte +split=validation  \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-python experiment.py +run_name="LowerCase" task=wsc +split=validation \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-python experiment.py +run_name="LowerCase" task="wsc.fixed" +split=validation  \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-python experiment.py +run_name="LowerCase" task=wic +split=validation  \
-  model_name="bigscience/T0_3B" \
-  evaluation.lowercase_choices=True
-
+python experiment.py +run_name="CrossTask" task=craigslist_bargains +split=validation \
+  num_proc=4 \
+  prompt_experiment_mode=cross_task \
+  prompt_path=prompts/general_fixed_choice.yaml \
+  model_name="bigscience/T0_3B"
 
 
 
