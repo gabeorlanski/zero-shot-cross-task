61cfe3099970582d776fb3823cc38c778e70df2a
Gabe <gabeorlanski@gmail.com>
2021-11-24 09:48:13 -0500

    LengthNorm Batches

--- gpu_experiment_batch.sh
+++ gpu_experiment_batch.sh
@@ -1,93 +1,117 @@
 #!/bin/bash
 # Run the experiments
-python experiment.py +run_name="T0" task=rte +split=validation model_name="bigscience/T0_3B"
-python experiment.py +run_name="T0" task=wsc +split=validation model_name="bigscience/T0_3B"
-python experiment.py +run_name="T0" task="wsc_fixed" +split=validation model_name="bigscience/T0_3B"
-python experiment.py +run_name="T0" task=wic +split=validation model_name="bigscience/T0_3B"
-
-python experiment.py +run_name="T5" task=cb +split=validation model_name="t5-3b" \
-  batch_size=1
-python experiment.py +run_name="T5" task=anli +split=dev_r1 model_name="t5-3b" \
-  batch_size=1
-python experiment.py +run_name="T5" task=anli +split=dev_r2 model_name="t5-3b" \
-  batch_size=1
-python experiment.py +run_name="T5" task=anli +split=dev_r3 model_name="t5-3b" \
-  batch_size=1
-python experiment.py +run_name="T5" task=rte +split=validation model_name="t5-3b" \
-  batch_size=1
-python experiment.py +run_name="T5" task=wsc +split=validation model_name="t5-3b" \
-  batch_size=1
-python experiment.py +run_name="T5" task="wsc_fixed" +split=validation model_name="t5-3b" \
-  batch_size=1
-python experiment.py +run_name="T5" task=wic +split=validation model_name="t5-3b" \
-  batch_size=1
-
-
-python experiment.py +run_name="CTBase" task=anli +split=dev_r1 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
+python experiment.py +run_name="T0LenNorm" task=rte +split=validation \
   model_name="bigscience/T0_3B" \
+  evaluation.length_normalization = True
+python experiment.py +run_name="T0LenNorm" task=wsc +split=validation \
+  model_name="bigscience/T0_3B" \
+  evaluation.length_normalization = True
+python experiment.py +run_name="T0LenNorm" task="wsc_fixed" +split=validation \
+  model_name="bigscience/T0_3B" \
+  evaluation.length_normalization = True
+python experiment.py +run_name="T0LenNorm" task=wic +split=validation \
+  model_name="bigscience/T0_3B" \
+  evaluation.length_normalization = True
+
+python experiment.py +run_name="T5LenNorm" task=cb +split=validation \
+  model_name="t5-3b" \
+  batch_size=1 \
+  evaluation.length_normalization=True
+python experiment.py +run_name="T5LenNorm" task=anli +split=dev_r1 \
+  model_name="t5-3b" \
+  batch_size=1 \
+  evaluation.length_normalization=True
+python experiment.py +run_name="T5LenNorm" task=anli +split=dev_r2 \
+  model_name="t5-3b" \
+  batch_size=1 \
+  evaluation.length_normalization=True
+python experiment.py +run_name="T5LenNorm" task=anli +split=dev_r3 \
+  model_name="t5-3b" \
+  batch_size=1 \
+  evaluation.length_normalization=True
+python experiment.py +run_name="T5LenNorm" task=rte +split=validation \
+  model_name="t5-3b" \
+  batch_size=1 \
+  evaluation.length_normalization=True
+python experiment.py +run_name="T5LenNorm" task=wsc +split=validation \
+  model_name="t5-3b" \
+  batch_size=1 \
+  evaluation.length_normalization=True
+python experiment.py +run_name="T5LenNorm" task="wsc_fixed" +split=validation \
+  model_name="t5-3b" \
+  batch_size=1 \
+  evaluation.length_normalization=True
+python experiment.py +run_name="T5LenNorm" task=wic +split=validation \
+  model_name="t5-3b" \
+  batch_size=1 \
   evaluation.length_normalization=True
 
-python experiment.py +run_name="CTBase" task=anli +split=dev_r2 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True
 
-python experiment.py +run_name="CTBase" task=anli +split=dev_r3 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True
-
-python experiment.py +run_name="CTBase" task=cb +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True
-
-python experiment.py +run_name="CTImplyContradict" task=anli +split=dev_r1 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']"
-
-python experiment.py +run_name="CTImplyContradict" task=anli +split=dev_r2 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']" \
-  batch_size=1
-
-python experiment.py +run_name="CTImplyContradict" task=anli +split=dev_r3 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']"
-
-python experiment.py +run_name="CTImplyContradict" task=cb +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']"
-
-python experiment.py +run_name="CTBase" task=craigslist_bargains +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=1
+#python experiment.py +run_name="CTBase" task=anli +split=dev_r1 \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True
+#
+#python experiment.py +run_name="CTBase" task=anli +split=dev_r2 \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True
+#
+#python experiment.py +run_name="CTBase" task=anli +split=dev_r3 \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True
+#
+#python experiment.py +run_name="CTBase" task=cb +split=validation \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True
+#
+#python experiment.py +run_name="CTImplyContradict" task=anli +split=dev_r1 \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True \
+#  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']"
+#
+#python experiment.py +run_name="CTImplyContradict" task=anli +split=dev_r2 \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True \
+#  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']" \
+#  batch_size=1
+#
+#python experiment.py +run_name="CTImplyContradict" task=anli +split=dev_r3 \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True \
+#  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']"
+#
+#python experiment.py +run_name="CTImplyContradict" task=cb +split=validation \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True \
+#  "task.preprocessor.choices=['Imply', 'Contradict', 'Neither']"
+#
+#python experiment.py +run_name="CTBase" task=craigslist_bargains +split=validation \
+#  num_proc=4 \
+#  prompt_experiment_mode=cross_task \
+#  prompt_path=prompts/general_fixed_choice.yaml \
+#  model_name="bigscience/T0_3B" \
+#  evaluation.length_normalization=True \
+#  batch_size=1
