435e3400711aa61127ce309702fc2edf684b8bd7
Gabe <gabeorlanski@gmail.com>
2021-11-25 11:43:42 -0500

    Typo

--- scripts/docker_gpu_1.sh
+++ scripts/docker_gpu_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run batch 2 in docker
-docker run --gpus device=0 --name batch_2 nnsem:latest bash scripts/batch_2.sh
+docker run --gpus device=1 --name batch_2 nnsem:latest bash scripts/batch_2.sh
 docker kill batch_2
 docker rm batch_2
