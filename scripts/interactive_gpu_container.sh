62bf18586fb23501273699bfbf75997e7d08d5a1
Gabe <gabeorlanski@gmail.com>
2021-11-25 12:12:32 -0500

    typo

--- scripts/interactive_gpu_container.sh
+++ scripts/interactive_gpu_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Open an interactive docker container, run it, then close it
-docker create --gpus device=$0 -it --name nlpproject-container nlpproject
+docker create --gpus device=1 -it --name nlpproject-container nlpproject
 docker start nlpproject-container
 docker exec -it nlpproject-container bash
 docker kill nlpproject-container
