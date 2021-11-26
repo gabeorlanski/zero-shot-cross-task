83c11ec8df37bc389c07d7ac317c5e3f5931137a
Gabe <gabeorlanski@gmail.com>
2021-11-26 16:44:18 -0500

    New Experiment Batch

--- scripts/batch_2.sh
+++ scripts/batch_2.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 # Run the experiments
-python experiment.py +run_name="CTBase" task=rte +split=validation \
+python experiment.py +run_name="CT_TNF" task=anli +split=dev_r3 \
   num_proc=4 \
   prompt_experiment_mode=cross_task \
   prompt_path=prompts/general_fixed_choice.yaml \
   model_name="bigscience/T0_3B" \
   evaluation.length_normalization=True \
-  batch_size=24 \
-  prompt_tasks=["BAREBONES"]
-  
+  batch_size=28 \
+  "task.preprocessor.choices=['True','Neither','False']"
+
+
+python experiment.py +run_name="CT_TNF" task=cb +split=validation \
+  num_proc=4 \
+  prompt_experiment_mode=cross_task \
+  prompt_path=prompts/general_fixed_choice.yaml \
+  model_name="bigscience/T0_3B" \
+  evaluation.length_normalization=True \
+  batch_size=28 \
+  "task.preprocessor.choices=['True','Neither','False']"
+
+python experiment.py +run_name="CT_ASN" task=anli +split=dev_r3 \
+  num_proc=4 \
+  prompt_experiment_mode=cross_task \
+  prompt_path=prompts/general_fixed_choice.yaml \
+  model_name="bigscience/T0_3B" \
+  evaluation.length_normalization=True \
+  batch_size=28 \
+  "task.preprocessor.choices=['Always','Sometimes','Never']"
+
+
+
+python experiment.py +run_name="CT_ASN" task=anli +split=dev_r2 \
+  num_proc=4 \
+  prompt_experiment_mode=cross_task \
+  prompt_path=prompts/general_fixed_choice.yaml \
+  model_name="bigscience/T0_3B" \
+  evaluation.length_normalization=True \
+  batch_size=28 \
+  "task.preprocessor.choices=['Always','Sometimes','Never']"
+
 python experiment.py +run_name="CT_INC" task=anli +split=dev_r3 \
   num_proc=4 \
   prompt_experiment_mode=cross_task \
@@ -16,14 +46,15 @@ python experiment.py +run_name="CT_INC" task=anli +split=dev_r3 \
   model_name="bigscience/T0_3B" \
   evaluation.length_normalization=True \
   batch_size=28 \
-  "task.preprocessor.choices=['Imply','Neither','Contradicts']"
+  "task.preprocessor.choices=['Imply','Neither','Contradict']"
 
 
-python experiment.py +run_name="CT_INC" task=cb +split=validation \
+
+python experiment.py +run_name="CT_INC" task=anli +split=dev_r2 \
   num_proc=4 \
   prompt_experiment_mode=cross_task \
   prompt_path=prompts/general_fixed_choice.yaml \
   model_name="bigscience/T0_3B" \
   evaluation.length_normalization=True \
   batch_size=28 \
-  "task.preprocessor.choices=['Imply','Neither','Contradicts']"
+  "task.preprocessor.choices=['Imply','Neither','Contradict']"
