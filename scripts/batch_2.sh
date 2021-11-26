74e1e6b072b1e5fa3ff047b47e072177f42d0c6c
Gabe <gabeorlanski@gmail.com>
2021-11-26 09:52:27 -0500

    Typo

--- scripts/batch_2.sh
+++ scripts/batch_2.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Run the experiments
+python experiment.py +run_name="CTBase" task=rte +split=validation \
+  num_proc=4 \
+  prompt_experiment_mode=cross_task \
+  prompt_path=prompts/general_fixed_choice.yaml \
+  model_name="bigscience/T0_3B" \
+  evaluation.length_normalization=True \
+  batch_size=24 \
+  prompt_tasks=["BAREBONES"]
+  
 python experiment.py +run_name="CT_INC" task=anli +split=dev_r3 \
   num_proc=4 \
   prompt_experiment_mode=cross_task \
@@ -9,14 +18,6 @@ python experiment.py +run_name="CT_INC" task=anli +split=dev_r3 \
   batch_size=28 \
   "task.preprocessor.choices=['Imply','Neither','Contradicts']"
 
-python experiment.py +run_name="CTBase" task=rte +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=24 \
-  prompt_tasks=["BAREBONES"]
 
 python experiment.py +run_name="CT_INC" task=cb +split=validation \
   num_proc=4 \
