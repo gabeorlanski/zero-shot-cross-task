df1d8c260c8154bdcd160e3de5e79562b96e601e
Gabe <gabeorlanski@gmail.com>
2021-12-06 08:30:55 -0500

    T0

--- scripts/batch_1.sh
+++ scripts/batch_1.sh
@@ -1,42 +1,10 @@
 #!/bin/bash
 # Run the experiments
-python experiment.py +run_name="CTOriginalChoices" task=aqua +split=validation \
+python experiment.py +run_name="T0" task=craigslist_bargains +split=validation \
   num_proc=4 \
   prompt_experiment_mode=cross_task \
   prompt_path=prompts/general_fixed_choice.yaml \
   model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
+  evaluation.length_normalization=False \
   batch_size=12 \
-  original_choice_str=True
-
-python experiment.py +run_name="CTOriginalChoices" task=cb +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=28 \
-  original_choice_str=True
-
-
-python experiment.py +run_name="CTOriginalChoices" task=anli +split=dev_r1 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=28 \
   dont_add_extra_text=True
-
-python experiment.py +run_name="CTOriginalChoices" task=wic +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=28 \
-  original_choice_str=True
-
-
-
-
--- scripts/batch_2.sh
+++ scripts/batch_2.sh
@@ -1,39 +1,10 @@
 #!/bin/bash
 # Run the experiments
-python experiment.py +run_name="CTOriginalChoices" task=anli +split=dev_r3 \
+python experiment.py +run_name="T5" task=craigslist_bargains +split=validation \
   num_proc=4 \
   prompt_experiment_mode=cross_task \
   prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=28 \
-  original_choice_str=True
-
-python experiment.py +run_name="CTOriginalChoices" task=anli +split=dev_r2 \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=28 \
-  original_choice_str=True
-
-
-python experiment.py +run_name="CTOriginalChoices" task=craigslist_bargains +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=12 \
-  original_choice_str=True
-
-
-python experiment.py +run_name="CTOriginalChoices" task=rte +split=validation \
-  num_proc=4 \
-  prompt_experiment_mode=cross_task \
-  prompt_path=prompts/general_fixed_choice.yaml \
-  model_name="bigscience/T0_3B" \
-  evaluation.length_normalization=True \
-  batch_size=28 \
-  original_choice_str=True
+  model_name="t5-3b" \
+  evaluation.length_normalization=False \
+  batch_size=6 \
+  dont_add_extra_text=True
